841780b312971b69117c484ed63393e81a337e66
Freeza <robercraudio@gmail.com>
2025-06-01 17:28:09 -0300

    adicao breadcrumb

--- obj/Debug/net9.0/DarkMarket.AssemblyInfo.cs
+++ obj/Debug/net9.0/DarkMarket.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DarkMarket")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d19bbd9defc341dd64a78dcd2263a28999731bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+412c9004bb0db050caf191cfb26db0a84275a3d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("DarkMarket")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DarkMarket")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
